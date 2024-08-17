d81562f9db00b30ec57b3d25d34074b4358d4c3b
Arushi Phadte <arushiphadte@gmail.com>
2024-08-17 12:59:07 -0400

    Add files via upload

--- IgnHack/webserver.py
+++ IgnHack/webserver.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from picamera2 import Picamera2, Preview
+from time import sleep
 
 app = Flask(__name__)
 
@@ -8,8 +10,28 @@ def home():
 
 @app.route("/TRACKER")
 def tracker():
-        return render_template('tracker.html')
+    return render_template('tracker.html')
 
+@app.route("/QUIZ")
+def quiz():
+    return render_template('quiz.html')
 
+@app.route("/FALL")
+def fall():
+    return render_template('fall.html')
+
+@app.route('/call-function')
+def call_function():
+    return feed()
+
+def feed():
+    camera = Picamera2()
+    camera.start_preview(Preview.QTGL)
+    sleep(2)
+    camera.start()
+    sleep(10)
+    camera.close()
+    return "Camera feed has been shown!"
+    
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
